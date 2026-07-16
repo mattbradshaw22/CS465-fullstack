f2e5da69395004b6b40ae4e9206e11e5ccc1aac8
Matthias Bradshaw <Matthias.bradshaw@snhu.edu>
2026-07-16 19:11:00 -0400

    make rooms page dynamic

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways'});
+    res.render('rooms', { title: 'Travlr Getaways', rooms: roomsData });
 };
 
 module.exports = {
